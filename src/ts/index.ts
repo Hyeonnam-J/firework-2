abc5a2602e4ac46d6c97e38c64beeeabce525c28
unknown <nami0879@naver.com>
2024-07-29 20:08:11 +0900

    RisingParticle y

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,8 +2,9 @@ import { x, y } from './canvas.js';
 import { play } from './play.js';
 import { ignite } from './processes/initiations/ignite.js';
 import { burst } from './processes/explosions/burst.js';
+import { RisingParticle } from './particles/rising_particle.js';
 
 play();
 
-ignite(x(50), y(50), 250, 0, 5, 'white', burst);
-ignite(x(50), y(0), 250, 90, 5, 'white', burst);
+// ignite(x(50), y(50), 250, 0, 5, 'white', burst);
+ignite(x(50), RisingParticle.y(), 250, 90, 5, 'white', burst);
--- src/ts/particles/rising_particle.ts
+++ src/ts/particles/rising_particle.ts
@@ -1,5 +1,7 @@
 import { BaseParticle } from "./particle.js";
+import { y } from "../canvas.js";
 
 export class RisingParticle extends BaseParticle {
-    
+    // The risingParticle's y starting point is always zero.
+    static y(): number { return y(0); }
 }
